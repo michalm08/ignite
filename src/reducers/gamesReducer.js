54a7cf17b3ddda564b2caedccb738ac3b27a6fd0
michalm08 <michalmski@gmail.com>
2021-01-27 20:35:20 +0100

    init redux-thunk

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,17 +1,24 @@
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+};
 
-const initState={
-    popular: [],
-    newGames: [],
-    upcoming:[]
-}
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'FETCH_GAMES':
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
 
-const gamesReducer = (state= initState, action) =>{
-    switch(action.type){
-        case "FETCH_GAMES":
-            return {...state}
-        default:
-            return {...state}
-    }
-}
+//ACTION
+const fetchGames = (userData) => {
+  return {
+    type: 'FETCH_GAMES',
+    payload: userData,
+  };
+};
 
 export default gamesReducer;
