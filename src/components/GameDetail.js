840af6b80851977587517f54aca93b357132a129
michalm08 <michalmski@gmail.com>
2021-01-28 17:13:54 +0100

    add rating stars

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -18,23 +18,9 @@ import nintendo from '../img/nintendo.svg';
 import apple from '../img/apple.svg';
 import gamepad from '../img/gamepad.svg';
 
-//GET PLATFORM IMAGES
-const getPlatform = (platform) => {
-  switch (platform) {
-    case 'PlayStation 4':
-      return playstation;
-    case 'Xbox One':
-      return xbox;
-    case 'PC':
-      return steam;
-    case 'Nintendo Switch':
-      return nintendo;
-    case 'iOS':
-      return apple;
-    default:
-      return gamepad;
-  }
-};
+//Star Images
+import starEmpty from '../img/star-empty.png';
+import starFull from '../img/star-full.png';
 
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
@@ -47,6 +33,39 @@ const GameDetail = ({ pathId }) => {
       history.push('/');
     }
   };
+
+  //Get Stars
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img alt='star' key={i} src={starFull}></img>);
+      } else {
+        stars.push(<img alt='star' key={i} src={starEmpty}></img>);
+      }
+    }
+    return stars;
+  };
+
+  //GET PLATFORM IMAGES
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case 'PlayStation 4':
+        return playstation;
+      case 'Xbox One':
+        return xbox;
+      case 'PC':
+        return steam;
+      case 'Nintendo Switch':
+        return nintendo;
+      case 'iOS':
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
+
   //Data
   const { screen, game, isLoading } = useSelector((state) => state.detail);
   return (
@@ -58,6 +77,7 @@ const GameDetail = ({ pathId }) => {
               <div className='raitng'>
                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
+                {getStars()}
               </div>
               <Info>
                 <h3>Platforms</h3>
@@ -135,6 +155,11 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`
